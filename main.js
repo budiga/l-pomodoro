f203b275175fd073ddd14fe454855ca84d5efe5a
liuxuehuan <cipao@2dfire.com>
2021-09-19 11:15:24 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain, Notification } = require('electron')
+const path = require('path')
+
+let mainWindow
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 360,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // 只加nodeIntegration不够，须加上这行才能正常运行
+    },
+    icon: path.join(__dirname, './assets/images/clock.png')
+  })
+  mainWindow.loadFile('./index.html')
+  // mainWindow.webContents.openDevTools()
+
+  if (process.platform === 'darwin') {
+    app.dock.setIcon(path.join(__dirname, './assets/images/clock.png'));
+  }
+
+  return mainWindow
+}
+
+function handleIPC() {
+  ipcMain.handle('notification', async (e, {body, title, actions, closeButtonText}) => {
+    let res = await new Promise((resolve, reject) => {
+      let notification = new Notification({
+        title,
+        body,
+        actions,
+        closeButtonText
+      })
+      notification.show()
+      notification.on('action', function(event) {
+        resolve({event: 'action'})
+      })
+      notification.on('close', function(event) {
+        resolve({event: 'close'})
+      })
+    })
+    return res
+  })
+}
+
+app.whenReady().then(() => {
+  handleIPC()
+  createMainWindow()
+})
